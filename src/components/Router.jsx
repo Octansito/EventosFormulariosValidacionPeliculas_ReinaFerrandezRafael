657caa6bfff8ecc868abfb9eec53684ebd1c210b
Octansito <octanecodlol@gmail.com>
2025-12-17 17:56:08 +0100

    Inicio actividad, adición botones

---
+++ src/components/Router.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Contenedor from "./Contenedor";
+import Detail from "./Detail";
+
+import Home from "../pages/Home";
+import Peliculas from "../pages/Peliculas";
+import Interpretes from "../pages/Interpretes";
+import Admin from "../pages/Admin";
+
+function AppRouter() {
+  return (
+    <Routes>
+      {/* Layout principal */}
+      <Route element={<Contenedor />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/inicio" element={<Navigate to="/" />} />
+        <Route path="/peliculas" element={<Peliculas />} />
+        <Route path="/interpretes" element={<Interpretes />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/detalle/pelicula/:idPeli" element={<Detail es="pelicula" />} />
+        <Route path="/detalle/pelicula/:idPeli/interprete/:idInterprete" element={<Detail es="interprete" />} />
+      </Route>
+
+      {/* Página 404 */}
+      <Route
+        path="*"
+        element={
+          <Contenedor titulo="Página no encontrada">
+            <p>La ruta no existe.</p>
+          </Contenedor>
+        }
+      />
+    </Routes>
+  );
+}
+export default AppRouter;
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,30 @@
+function Admin() {
+  return (
+    <>
+      <section>
+        <h2 className="text-xl font-semibold mb-4">Panel de Administración</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full mt-8">
+          <button
+            type="button"
+            className="w-full px-4 py-3 text-grey-600 border border-transparent rounded-lg bg-blue-50 hover:border-blue-500 hover:bg-blue-100 hover:text-blue-700 transition"
+          >
+            Añadir Película (Controlado)
+          </button>
+          <button
+            type="button"
+            className="w-full px-4 py-3 text-grey-600 border border-transparent rounded-lg bg-blue-50 hover:border-blue-500 hover:bg-blue-100 hover:text-blue-700 transition"
+          >
+            Añadir Película (No Controlado)
+          </button>
+          <button
+            type="button"
+            className="w-full px-4 py-3 text-grey-600 border border-transparent rounded-lg bg-blue-50 hover:border-blue-500 hover:bg-blue-100 hover:text-blue-700 transition"
+          >
+            Añadir Intérprete
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Admin;
